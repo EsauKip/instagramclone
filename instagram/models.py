22632686f6d4fa3315cb7a49ef3de4762181d1a3
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-01 16:39:51 +0300

    add databae

--- instagram/models.py
+++ instagram/models.py
@@ -7,4 +7,6 @@ class Post(models.Model):
     image=models.ImageField(blank=True,null=True)
     caption=models.TextField()
     date_created=models.DateTimeField(default=timezone.now)
-      
+
+    def __str__(self):
+        return self.caption 
