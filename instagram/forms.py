2a0e63818bf92ed84843ddc6b04354a56972fafb
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-02 23:33:19 +0300

    create update user form,aff email field

--- instagram/forms.py
+++ instagram/forms.py
@@ -4,3 +4,12 @@ from instagram.models import Comment, Post, Profile
 from django.contrib.auth.forms import UserCreationForm
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required.')
+    class Meta:
+        model = User
+        fields = ('username', 'email')        
