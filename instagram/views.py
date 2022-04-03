fc1cc54ef6f9a72f76929709f87275d93e35c974
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-03 09:17:22 +0300

    add signup functionality

--- instagram/views.py
+++ instagram/views.py
@@ -15,3 +15,22 @@ def display_home(request):
     profile = Profile.objects.all()
     comment = Comment.objects.all()
     return render(request,'index.html',{"posts":posts,"profile":profile,"comment":comment})
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST['username']
+            password = request.POST['password']
+            email = request.POST['email']
+
+            user = User.objects.create_user(username=username, email=email,password=password)
+            subject = 'welcome to IC page'
+            message = f'Hi {user.username}, thank you for registering in instagram clone.'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [user.email, ]
+            send_mail( subject, message, email_from, recipient_list )
+            return HttpResponse('Thank you for registering with us')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration_form.html', {'form': form})
